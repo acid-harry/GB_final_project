7dfade362408715dd8cc751da6e419cfbca8af9f
Krivoukhov Igor <igorkrivoukhov@gmail.com>
2022-10-31 10:47:54 +0300

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arr1 = new string[5] {"123", "234", "gb", "helloworld", ":)"};
+string[] arr2 = new string[arr1.Length];
+void SecondArrayWithIF(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
